747a7809ac1880375842a95c6736050f630d9340
moaalaa <moaalaa16@gmail.com>
2020-01-07 08:01:29 +0200

    Adding About Us API Route

--- server.js
+++ server.js
@@ -6,5 +6,9 @@ app.get('/', (req, res) => {
     res.send('App Installed!!!');
 });
 
+app.get('/about-us', (req, res) => {
+    res.send('About API');
+});
+
 app.listen(3000, () => console.log('Server Is Live On Port 3000'));
 
